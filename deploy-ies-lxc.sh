b1830638c9310500dfb20f171a0a50b52ce8ce61
DXCSithlordPadawan <bofh2bikerider@gmail.com>
2025-09-01 10:02:06 +0100

    Add files via upload

--- deploy-ies-lxc.sh
+++ deploy-ies-lxc.sh
@@ -462,7 +462,7 @@ install_docker() {
     
     pct exec $CT_ID -- bash -c "
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
         apt-get update
         apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
         systemctl enable docker
